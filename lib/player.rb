280d7454b11b0c412d4a6f880d1df17fbdc5d1b6
TunaSandwinch <winch.mascarenas@gmail.com>
2024-08-24 17:23:40 +0800

    added all winning conditions

--- lib/player.rb
+++ lib/player.rb
@@ -32,4 +32,52 @@ class Player
     end
     result
   end
+
+  def left_diagonal_result(list)
+    result = []
+    list_index = 0
+    3.times do
+      result.push(list[list_index])
+      list_index += 4
+    end
+    result
+  end
+
+  def right_diagonal_result(list)
+    result = []
+    list_index = 2
+    3.times do
+      result.push(list[list_index])
+      list_index += 2
+    end
+    result
+  end
+
+  def vertical_win?(list)
+    winning_combs = vertical_result(list)
+    winning_combs.each do |winning_comb|
+      return true if winning_comb == [piece, piece, piece]
+    end
+    false
+  end
+
+  def horizontal_win?(list)
+    winning_combs = horizontal_result(list)
+    winning_combs.each do |winning_comb|
+      return true if winning_comb == [piece, piece, piece]
+    end
+    false
+  end
+
+  def right_diagonal_win?(list)
+    return true if right_diagonal_result(list) == [piece, piece, piece]
+
+    false
+  end
+
+  def left_diagonal_win?(list)
+    return true if left_diagonal_result(list) == [piece, piece, piece]
+
+    false
+  end
 end
