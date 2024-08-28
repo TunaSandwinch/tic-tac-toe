76bd1567bb11c54f3aafafed077919ce3b0ed499
TunaSandwinch <winch.mascarenas@gmail.com>
2024-08-28 18:09:55 +0800

    re-structure the onjects player and play_game

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,54 +1,67 @@
 # frozen_string_literal: true
 
-# all the things to navigate the game and also to get board information goes here
+# Things needed to play the game goes here
 class PlayGame
-  attr_accessor :piece
+  attr_accessor :player1, :player2, :board, :current_player
 
-  def initialize(type)
-    @piece = type
+  def initialize
+    @player1 = 'X'
+    @player2 = 'O'
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @current_player = @player1
   end
 
-  def valid_move?(board, position)
-    if position.instance_of?(Integer) && position.between?(1, 9)
-      position -= 1
-      board[position].instance_of?(Integer)
-    else
-      false
-    end
-  end
-
-  def player_move(type, board, position)
-    board[position.to_i] = type
-  end
-
-  def self.display_board(board)
+  def display_board # rubocop:disable Metrics/AbcSize
+    puts ''
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts '-----------'
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts '-----------'
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts ''
   end
 
-  private
+  def player_move
+    puts 'take your move:'
+    gets.chomp
+  end
 
-  def horizontal_result(list)
+  def make_move
+    move = player_move.to_i - 1
+    if move.instance_of?(Integer) && move.between?(0, 8) && board[move].instance_of?(Integer)
+      board[move] = current_player
+    else
+      p 'Invalid move! Try Again!'
+      make_move
+    end
+  end
+
+  def set_current_player
+    self.current_player = if current_player == player2
+                            player1
+                          else
+                            player2
+                          end
+  end
+
+  def horizontal_result
     result = [[], [], []]
     list_index = 0
     result.each do |item|
       3.times do
-        item.push(list[list_index])
+        item.push(board[list_index])
         list_index += 1
       end
     end
     result
   end
 
-  def vertical_result(list)
+  def vertical_result
     result = [[], [], []]
     list_index = 0
     result.each do |item|
       3.times do
-        item.push(list[list_index])
+        item.push(board[list_index])
         list_index += 3
       end
       list_index -= 8
@@ -56,21 +69,21 @@ class PlayGame
     result
   end
 
-  def left_diagonal_result(list)
+  def left_diagonal_result
     result = []
     list_index = 0
     3.times do
-      result.push(list[list_index])
+      result.push(board[list_index])
       list_index += 4
     end
     result
   end
 
-  def right_diagonal_result(list)
+  def right_diagonal_result
     result = []
     list_index = 2
     3.times do
-      result.push(list[list_index])
+      result.push(board[list_index])
       list_index += 2
     end
     result
--- lib/player.rb
+++ lib/player.rb
@@ -1,32 +1,42 @@
 # frozen_string_literal: true
 
 require_relative 'play_game'
-# all the necessarry things to monitor the game state of each player goes here
+
+# will determine wether a player wins as well if two players tied
 class Player < PlayGame
-  def vertical_win?(list)
-    winning_combs = vertical_result(list)
+  def player_win?
+    return true if vertical_win? || horizontal_win? ||
+                   right_diagonal_win? || left_diagonal_win?
+
+    false
+  end
+
+  private
+
+  def vertical_win?
+    winning_combs = vertical_result
     winning_combs.each do |winning_comb|
-      return true if winning_comb == [piece, piece, piece]
+      return true if winning_comb == [current_player, current_player, current_player]
     end
     false
   end
 
-  def horizontal_win?(list)
-    winning_combs = horizontal_result(list)
+  def horizontal_win?
+    winning_combs = horizontal_result
     winning_combs.each do |winning_comb|
-      return true if winning_comb == [piece, piece, piece]
+      return true if winning_comb == [current_player, current_player, current_player]
     end
     false
   end
 
-  def right_diagonal_win?(list)
-    return true if right_diagonal_result(list) == [piece, piece, piece]
+  def right_diagonal_win?
+    return true if right_diagonal_result == [current_player, current_player, current_player]
 
     false
   end
 
-  def left_diagonal_win?(list)
-    return true if left_diagonal_result(list) == [piece, piece, piece]
+  def left_diagonal_win?
+    return true if left_diagonal_result == [current_player, current_player, current_player]
 
     false
   end
